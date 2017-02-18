696d958c6c6f8257e9194fe43ee55aabc203bad4
rushan <seif.rushan@gmail.com>
2017-02-19 00:02:14 +0300

    show all files from directory and subdirectories

--- arch.c
+++ arch.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
 
-
-void main() {
+void arch(char *path, char *subdir) {
 	
-	char path[100];
 	DIR *dir;
 	struct dirent *entry;
+	struct stat statbuf;
 
-	printf("directory to archiving: ");
-	scanf("%s", path);
-	
 	if ((dir = opendir(path)) == NULL) {
 		printf("cannot open directory\n");
 		exit(0);	
 	}
  	
-	while ((entry = readdir(dir)) != NULL) {
-		printf("%s\n", entry->d_name);
-	}
+	chdir(path);
+	printf("START %s\n", subdir);
 
-	printf("----end----\n"); 
+	while ((entry = readdir(dir)) != NULL) {
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+			continue;
+		lstat(entry->d_name, &statbuf);
+		
+		if (S_ISDIR(statbuf.st_mode))  {
+			arch(entry->d_name, entry->d_name);
+		}
+		else printf("%s\n", entry->d_name);
+	}
+	closedir(dir);
+	chdir("..");
+	printf("END\n");
+}
+
+
+void main() {
+	char path[1000];
+	printf("directory to archiving: ");
+	scanf("%s", path);
+	arch(path, strrchr(path, '/') + 1);
+	printf("----end----\n");
 }
 
