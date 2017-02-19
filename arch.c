ec792ae666b030336cd2c62a160ff5256e5539aa
rushan <seif.rushan@gmail.com>
2017-02-19 23:13:02 +0300

    fix memory leak and coding style

--- arch.c
+++ arch.c
@@ -10,38 +10,37 @@
 FILE *archive;
 
 
-void pack(char *path, char *dname) {
-	
-	DIR *dir;
+void pack(char *path, char *dname)
+{
 	struct dirent *entry;
 	struct stat statbuf;
+	DIR *dir = opendir(path);
 
-	if ((dir = opendir(path)) == NULL) {
+	if (dir == NULL) {
 		printf("cannot open directory\n");
-		exit(0);	
+		exit(0);
 	}
- 	
 	chdir(path);
 	fwrite("{", 1, 1, archive);
 	fwrite(dname, 1, 255, archive);
-
-	while ((entry = readdir(dir)) != NULL) {
-		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+	while (entry = readdir(dir)) {
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
 			continue;
-
 		lstat(entry->d_name, &statbuf);
-		
-		if (S_ISDIR(statbuf.st_mode))  
-			pack(entry->d_name, entry->d_name);	
+
+		if (S_ISDIR(statbuf.st_mode))
+			pack(entry->d_name, entry->d_name);
 		else {
 			fwrite("f", 1, 1, archive);
 			fwrite(&statbuf.st_size, sizeof(size_t), 1, archive);
 			fwrite(&entry->d_name, 1, 255, archive);
 			FILE *file = fopen(entry->d_name, "rb");
 			char *buf = malloc(statbuf.st_size);
+
 			fread(buf, 1, statbuf.st_size, file);
 			fwrite(buf, 1, statbuf.st_size, archive);
 			fclose(file);
+			free(buf);
 		}
 	}
 	fwrite("}", 1, 1, archive);
@@ -49,49 +48,51 @@ void pack(char *path, char *dname) {
 	chdir("..");
 }
 
-
-void unpack(char *path) {
+void unpack(char *path)
+{
 	char name[255];
 	char flag;
 	size_t n;
+
 	chdir(path);
 	while ((n = fread(&flag, 1, 1, archive)) != 0) {
 		if (flag == '{') {
 			fread(name, 1, 255, archive);
 			mkdir(name, S_IRUSR|S_IWUSR|S_IXUSR);
 			chdir(name);
-		}
-		else if (flag == '}') {
+		} else if (flag == '}') {
 			chdir("..");
-		}
-		else {
+		} else {
 			size_t size;
+
 			FILE *file;
 			size_t nb;
+
 			fread(&size, sizeof(size_t), 1, archive);
 			fread(name, 1, 255, archive);
 			file = fopen(name, "wb");
 			char *buf = malloc(size);
+
 			fread(buf, 1, size, archive);
 			fwrite(buf, 1, size, file);
 			fclose(file);
+			free(buf);
 		}
 	}
 }
 
-
-void main(int argc, char* argv[]) {
+void main(int argc, char *argv[])
+{
 	if (argc == 3 && strcmp(argv[1], "pack") == 0) {
 		char *name = strrchr(argv[2], '/') + 1;
+
 		archive = fopen(name, "wb");
 		pack(argv[2], name);
 		fclose(archive);
-	}
-	else if (argc == 4 && strcmp(argv[1], "unpack") == 0) {
+	} else if (argc == 4 && strcmp(argv[1], "unpack") == 0) {
 		archive = fopen(argv[2], "rb");
 		unpack(argv[3]);
 		fclose(archive);
-	}
-	else printf("wrong parameters!\n arch pack [directory to packaging]\narch unpack [archive file] [path for unpackaging]\n");
+	} else
+		printf("wrong parameters!\n arch pack [directory to packaging]\narch unpack [archive file] [path for unpackaging]\n");
 }
-
