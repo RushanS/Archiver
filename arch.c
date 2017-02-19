7a8b02fbc0e9f1ac892cea70729b11ec5336c442
rushan <seif.rushan@gmail.com>
2017-02-19 19:13:35 +0300

    work version

--- arch.c
+++ arch.c
@@ -5,7 +5,11 @@
 #include <dirent.h>
 #include <string.h>
 
-void arch(char *path, char *subdir) {
+
+FILE *archive;
+
+
+void arch(char *path, char *dname) {
 	
 	DIR *dir;
 	struct dirent *entry;
@@ -17,29 +21,84 @@ void arch(char *path, char *subdir) {
 	}
  	
 	chdir(path);
-	printf("START %s\n", subdir);
+	printf("{%s\n", dname);
+	
+	fwrite("{", 1, 1, archive);
+	fwrite(dname, 1, 255, archive);
 
 	while ((entry = readdir(dir)) != NULL) {
 		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
 			continue;
+
 		lstat(entry->d_name, &statbuf);
 		
-		if (S_ISDIR(statbuf.st_mode))  {
-			arch(entry->d_name, entry->d_name);
+		if (S_ISDIR(statbuf.st_mode))  
+			arch(entry->d_name, entry->d_name);	
+		else {
+			printf("%ld%s\n", statbuf.st_size, entry->d_name);
+			fwrite("f", 1, 1, archive);
+			fwrite(&statbuf.st_size, sizeof(size_t), 1, archive);
+			fwrite(&entry->d_name, 1, 255, archive);
+			FILE *file = fopen(entry->d_name, "rb");
+			char buf[1];
+			size_t nb;
+			while ((nb = fread(buf, 1, statbuf.st_size, file)) != 0) {
+				fwrite(buf, 1, statbuf.st_size, archive);
+			}
+			fclose(file);
 		}
-		else printf("%s\n", entry->d_name);
 	}
+	printf("}\n");
+	fwrite("}", 1, 1, archive);
 	closedir(dir);
 	chdir("..");
-	printf("END\n");
 }
 
 
-void main() {
-	char path[1000];
-	printf("directory to archiving: ");
-	scanf("%s", path);
-	arch(path, strrchr(path, '/') + 1);
-	printf("----end----\n");
+void unarch(char *path) {
+	char name[255];
+	char flag;
+	size_t n;
+	chdir(path);
+	while ((n = fread(&flag, 1, 1, archive)) != 0) {
+		if (flag == '{') {
+			fread(name, 1, 255, archive);
+			mkdir(name, S_IRUSR|S_IWUSR|S_IXUSR);
+			chdir(name);
+		}
+		else if (flag == '}') {
+			chdir("..");
+		}
+		else {
+			size_t size;
+			FILE *file;
+			char buf[1];
+			size_t nb;
+			fread(&size, sizeof(size_t), 1, archive);
+			fread(name, 1, 255, archive);
+			file = fopen(name, "wb");
+			fread(buf, 1, size, archive);
+			fwrite(buf, 1, size, file);
+			fclose(file);
+		}
+	}
+}
+
+
+void main(int argc, char* argv[]) {
+	if (argc == 4) {
+		if (strcmp(argv[1], "pack") == 0) {
+			archive = fopen(argv[3], "wb");
+			arch(argv[2], strrchr(argv[2], '/') + 1);
+			fclose(archive);
+		}
+		else if (strcmp(argv[1], "unpack") == 0) {
+			archive = fopen(argv[2], "rb");
+			unarch(argv[3]);
+			fclose(archive);
+		}
+		else printf("wrong parameters! \n arch pack [directory to packaging] [path for archive file including file name]\n arch unpack [path for archive file] [path for unpackaging]\n");
+	}
+	else printf("wrong number of arguments! \n arch pack [directory to packaging] [path for archive file including file name]\n arch unpack [path for archive file] [path for unpackaging]\n");
 }
 
