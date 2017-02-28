499887860a1d079fdfb61ecd05f403791a6edb30
rushan <seif.rushan@gmail.com>
2017-02-28 23:31:29 +0300

    update arch.c

--- arch.c
+++ arch.c
@@ -31,12 +31,14 @@ void pack(char *path, char *dname)
 		if (S_ISDIR(statbuf.st_mode))
 			pack(entry->d_name, entry->d_name);
 		else {
+			FILE *file;
+			char *buf;
+
 			fwrite("f", 1, 1, archive);
 			fwrite(&statbuf.st_size, sizeof(size_t), 1, archive);
 			fwrite(&entry->d_name, 1, 255, archive);
-			FILE *file = fopen(entry->d_name, "rb");
-			char *buf = malloc(statbuf.st_size);
-
+			file = fopen(entry->d_name, "rb");
+			buf = malloc(statbuf.st_size);
 			fread(buf, 1, statbuf.st_size, file);
 			fwrite(buf, 1, statbuf.st_size, archive);
 			fclose(file);
@@ -58,21 +60,20 @@ void unpack(char *path)
 	while ((n = fread(&flag, 1, 1, archive)) != 0) {
 		if (flag == '{') {
 			fread(name, 1, 255, archive);
-			mkdir(name, S_IRUSR|S_IWUSR|S_IXUSR);
+			mkdir(name, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH);
 			chdir(name);
 		} else if (flag == '}') {
 			chdir("..");
 		} else {
 			size_t size;
-
-			FILE *file;
 			size_t nb;
+			FILE *file;
+			char *buf;
 
 			fread(&size, sizeof(size_t), 1, archive);
 			fread(name, 1, 255, archive);
 			file = fopen(name, "wb");
-			char *buf = malloc(size);
-
+			buf = malloc(size);
 			fread(buf, 1, size, archive);
 			fwrite(buf, 1, size, file);
 			fclose(file);
@@ -85,14 +86,20 @@ void main(int argc, char *argv[])
 {
 	if (argc == 3 && strcmp(argv[1], "pack") == 0) {
 		char *name = strrchr(argv[2], '/') + 1;
+		char filename[255];
 
-		archive = fopen(name, "wb");
+		strncat(filename, name, 250);
+		strncat(filename, ".arch", 5);
+		archive = fopen(filename, "wb");
 		pack(argv[2], name);
 		fclose(archive);
 	} else if (argc == 4 && strcmp(argv[1], "unpack") == 0) {
 		archive = fopen(argv[2], "rb");
 		unpack(argv[3]);
 		fclose(archive);
-	} else
-		printf("wrong parameters!\n arch pack [directory to packaging]\narch unpack [archive file] [path for unpackaging]\n");
+	} else {
+		printf("wrong parameters!\n");
+		printf("arch pack [directory to packaging]\n");
+		printf("arch unpack [archive file] [path for unpackaging]\n");
+	}
 }
